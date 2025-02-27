475ce777cf061a4928faec1278c2d3e4ea635074
tushcmd <muturidavid854@gmail.com>
2025-02-27 23:42:19 +0300

    Remove .history from tracking

--- .history/app/page_20250227230944.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <div>
-        <span className='text-gray-800'>Hello world</span>
-      </div>
-    </main>
-  );
-}
--- .history/next.config_20250227231042.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/.' : '',
-  output: 'export',
-  trailingSlash: true,
-};
-
-export default nextConfig;
