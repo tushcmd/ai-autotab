f8ad11f1c5f9e527e9dbc162282b064849cf858a
tushcmd <muturidavid854@gmail.com>
2025-03-02 12:51:23 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/.' : '',
-  output: 'export',
+  assetPrefix: process.env.NODE_ENV === "production" ? "/." : "",
+  output: "export",
+  // distDir: 'out',
   trailingSlash: true,
+  // Disable image optimization since it's not needed for extensions
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
