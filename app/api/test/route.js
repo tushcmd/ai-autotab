da4662c01ec34c0477755b894a911e84a3c53f4d
tushcmd <muturidavid854@gmail.com>
2025-03-03 21:30:19 +0300

    del

--- app/api/test/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET(req) {
-  return new Response(JSON.stringify({ message: "API is working" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
